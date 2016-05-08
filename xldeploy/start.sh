8d12b1a2c4b473418b25d76e8b6bb653b8a771a2
christophelec <christophe.lecointe@tuta.io>
2016-05-08 18:22:45 +0200

    add nohup stuff

--- xldeploy/start.sh
+++ xldeploy/start.sh
@@ -2,6 +2,6 @@
 
 echo "Starting the XLD container"
 
-nohup docker run -p 4516:4516 --name xldeploy xldeploy 2>&1 > xld.log
+nohup docker run -p 4516:4516 --name xldeploy xldeploy 2>&1 > xld.log &
 
 exit $?
